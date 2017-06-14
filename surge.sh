bb278f5e770abab02382ffcb7262b608761aac88
Leonardo Dino <leonardo@kunstdesign.com.br>
2017-06-14 07:28:21 -0300

    improve bash help

--- surge.sh
+++ surge.sh
@@ -1,25 +1,16 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-if [ ! -z "$HS_SHORT_HELP" ]; then
-	echo 'deploy project to surge.sh'
-	exit 0
-fi
+if [ ! -z "$HS_HELP" ]; then
+	[ ! -z "$HS_SHORT_HELP" ] && printf '%s\n' \
+		'deploy project to surge.sh';
 
-if [ ! -z "$HS_USAGE_HELP" ]; then
-	echo '{COMMAND} [path]'
-	echo 'use the argument to set a custom input folder'
-	exit 0
-fi
+	[ ! -z "$HS_USAGE_HELP" ] && printf '%s\n' '{COMMAND} [path]' \
+		'use the argument to set a custom input folder';
 
-if [ ! -z "$HS_VARIABLES_HELP" ]; then
-	echo 'DOMAIN: set surge.sh domain'
-	exit 0
-fi
-
-if [ ! -z "$HS_HIDE_HELP" ]; then
-	exit 0
-fi
+	[ ! -z "$HS_VARIABLES_HELP" ] && printf '%s\n' \
+		'DOMAIN: set surge.sh domain';
+exit 0; fi;
 
 defaultfolder='./www'
 folder="${1:-$defaultfolder}"
